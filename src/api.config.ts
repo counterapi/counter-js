f22d5bf5b66827428a4f590025c9756fd2819233
omegion <kurtulus@mail.com>
2021-03-27 15:41:09 +0100

    Fix CORS issue

--- src/api.config.ts
+++ src/api.config.ts
@@ -1,23 +1,21 @@
 import * as qs from "qs";
-import { PathLike } from "fs";
 
 const BASE_URL = "https://api.counterapi.dev/v1/";
 
 export const apiConfig = {
   returnRejectedPromiseOnError: true,
-  withCredentials: true,
+  withCredentials: false,
   timeout: 30000,
   baseURL: BASE_URL,
   headers: {
     common: {
-      "Cache-Control": "no-cache, no-store, must-revalidate",
-      Pragma: "no-cache",
       "Content-Type": "application/json",
       Accept: "application/json",
     },
   },
-  paramsSerializer: (params: PathLike) =>
-    qs.stringify(params, { indices: false }),
+  paramsSerializer: function (params) {
+    return qs.stringify(params, { indices: false });
+  },
   validateStatus: function (status) {
     return status == 200;
   },
