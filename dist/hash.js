fb5084fc228cfbe1b47cb18563ff4216ad10810b
omegion <kurtulus@mail.com>
2021-03-31 23:46:44 +0200

    Bump to v0.17.0

--- dist/hash.js
+++ dist/hash.js
@@ -1,12 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Hash = void 0;
-var crypto = require("crypto");
+var crypto_js_1 = require("crypto-js");
 var Hash = (function () {
     function Hash() {
     }
     Hash.create = function (str) {
-        return crypto.createHash("sha256").update(str).digest("hex");
+        return crypto_js_1.default.createHash("sha256").update(str).digest("hex");
     };
     return Hash;
 }());
