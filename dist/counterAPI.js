e4cadd3fd90617733d5c110e3471c43dfb42471a
Hakan <hakan@kurtulus.dev>
2023-10-07 12:05:05 +0200

    feat: Enable renovate bot

--- dist/counterAPI.js
+++ dist/counterAPI.js
@@ -14,7 +14,7 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
     function verb(n) { return function (v) { return step([n, v]); }; }
     function step(op) {
         if (f) throw new TypeError("Generator is already executing.");
-        while (_) try {
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
             if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
             if (y = 0, t) op = [op[0] & 2, t.value];
             switch (op[0]) {
@@ -48,12 +48,12 @@ var GroupByTypes;
     GroupByTypes["Week"] = "week";
     GroupByTypes["Month"] = "month";
     GroupByTypes["Year"] = "year";
-})(GroupByTypes = exports.GroupByTypes || (exports.GroupByTypes = {}));
+})(GroupByTypes || (exports.GroupByTypes = GroupByTypes = {}));
 var OrderByTypes;
 (function (OrderByTypes) {
     OrderByTypes["ASC"] = "asc";
     OrderByTypes["DESC"] = "desc";
-})(OrderByTypes = exports.OrderByTypes || (exports.OrderByTypes = {}));
+})(OrderByTypes || (exports.OrderByTypes = OrderByTypes = {}));
 var CounterAPI = (function () {
     function CounterAPI() {
         this.axios = axios_1.default.create(api_config_1.apiConfig);
